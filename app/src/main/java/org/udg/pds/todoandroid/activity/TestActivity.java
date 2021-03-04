b62af80da209cc900de1d92172d71bfa21486583
Hamza Saddouki <hsaddouki@gmai.com>
2021-03-04 22:57:34 +0100

    Arreglos en el diseño

--- app/src/main/java/org/udg/pds/todoandroid/activity/TestActivity.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/TestActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.udg.pds.todoandroid.R;
 
@@ -14,9 +17,14 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        final EditText et = (EditText) findViewById(R.id.editText);
+        Button btn = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TestActivity.this, et.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-    Button button2 = findViewById(R.id.button2);
-
-
 }
